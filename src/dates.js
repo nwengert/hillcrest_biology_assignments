177a09739e38cfc5a7fd25b3054d6f389c1456fa
Nathan Wengert <nate.weng@gmail.com>
2019-01-30 17:18:09 -0700

    need to round days DOWN for correct math

--- src/dates.js
+++ src/dates.js
@@ -2,7 +2,7 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-var platyNotesDate = new Date(2019, 0, 10)
+var platyNotesDate = new Date(2019, 0, 20)
 console.log(months[platyNotesDate.getMonth()] + " " + platyNotesDate.getDay())
 
 console.log(platyNotesDate.toDateString())
@@ -10,19 +10,20 @@ console.log(platyNotesDate.toDateString())
 var today = new Date()
 console.log("Today is: " + today.toDateString())
 
-console.log(platyNotesDate.getDate())
-
 Date.daysBetween = function( date1, date2 ) {
     //Get 1 day in milliseconds
     var one_day=1000*60*60*24;
   
     // Convert both dates to milliseconds
-    var date1_ms = date1.getTime();
-    var date2_ms = date2.getTime();
+    var today_ms = today.getTime();
+    var platyNotesDate_ms = platyNotesDate.getTime();
   
     // Calculate the difference in milliseconds
-    var difference_ms = date2_ms - date1_ms;
+    var difference_ms = today_ms - platyNotesDate_ms;
       
     // Convert back to days and return
-    return Math.round(difference_ms/one_day); 
+    return Math.round(difference_ms/one_day);      //get this to round down?
   }
+  console.log("today day = " + today.getDate())
+  console.log("assigned day = " + platyNotesDate.getDate())
+  console.log("difference = " + Date.daysBetween())
